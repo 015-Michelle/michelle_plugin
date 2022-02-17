66a7f44439870d616733305f0528408d3f45c545
tm <tm@work.com>
2022-02-17 11:32:01 +0800

    add color_util

---
+++ lib/utils/color_util.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ColorUtil {
+  static Color parseRGBA(String rgba) {
+    try {
+      var rgbaValues =
+          rgba.split('(').last.split(')').first.split((',')).map((e) => e.trim()).toList();
+      return Color.fromARGB(
+          int.parse(rgbaValues[0]),
+          int.parse(
+            rgbaValues[1],
+          ),
+          int.parse(rgbaValues[2]),
+          int.parse(rgbaValues[3]));
+    } catch (e) {
+      return Colors.transparent;
+    }
+  }
+}
