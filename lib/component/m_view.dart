a16bfc1139ba7275a066a413e6d7cca3f3f62d7c
tm <tm@work.com>
2022-02-18 10:37:26 +0800

    finish m_view

--- lib/component/m_view.dart
+++ lib/component/m_view.dart
@@ -1,1 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:michelle_plugin/model/m_page_model.dart';
 
+import 'm_page.dart';
+
+class MView extends StatefulWidget {
+  final Map<String, dynamic> config;
+  final bool? isFullPage;
+
+  const MView({Key? key, required this.config, this.isFullPage = false}) : super(key: key);
+
+  @override
+  _MViewState createState() => _MViewState();
+}
+
+class _MViewState extends State<MView> {
+  Map<String, dynamic> get config => widget.config;
+  bool? get isFullPage => widget.isFullPage;
+
+  late MPageModel pageModel;
+
+  @override
+  Widget build(BuildContext context) {
+    pageModel = MPageModel.fromJson(config);
+
+    return MPage(
+      pageModel: pageModel,
+      isFullPage: isFullPage ?? false,
+    );
+  }
+}
