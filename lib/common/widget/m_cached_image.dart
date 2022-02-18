b1e072099459c2e7d90eda05c5ac5f73ccbd6ddc
tm <tm@work.com>
2022-02-18 15:28:01 +0800

    add mItemBorderRadius

--- lib/common/widget/m_cached_image.dart
+++ lib/common/widget/m_cached_image.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:michelle_plugin/constant/m_transparent_image.dart';
+import 'package:michelle_plugin/model/m_style_model.dart';
 import 'package:michelle_plugin/utils/m_local_cache_network_image.dart';
 
 class MCachedImage extends StatelessWidget {
@@ -10,6 +11,7 @@ class MCachedImage extends StatelessWidget {
   final Color? color;
   final BoxFit? boxFit;
   final bool isShowError;
+  final MItemBorderRadius? mItemBorderRadius;
 
   const MCachedImage({
     Key? key,
@@ -20,30 +22,41 @@ class MCachedImage extends StatelessWidget {
     this.color,
     this.boxFit,
     required this.isShowError,
+    this.mItemBorderRadius,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: color,
-      width: imgWidth?.toDouble(),
-      height: imgHeight?.toDouble(),
-      child: Image(
-        image: MLocalCacheNetworkImage(url),
-        loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
-          return loadingProgress == null ? child : Image.memory(mTransparentImage);
-        },
-        errorBuilder: (BuildContext context, Object exception, StackTrace? stackTrace) {
-          return isShowError
-              ? Center(
-                  child: Icon(
-                    Icons.error,
-                    color: Colors.black.withOpacity(0.08),
-                  ),
-                )
-              : Container();
-        },
-        fit: boxFit ?? BoxFit.cover,
+    return ClipRRect(
+      borderRadius: mItemBorderRadius == null
+          ? BorderRadius.zero
+          : BorderRadius.only(
+              topLeft: Radius.circular(mItemBorderRadius!.topLeft.toDouble()),
+              topRight: Radius.circular(mItemBorderRadius!.topRight.toDouble()),
+              bottomLeft: Radius.circular(mItemBorderRadius!.bottomLeft.toDouble()),
+              bottomRight: Radius.circular(mItemBorderRadius!.bottomRight.toDouble()),
+            ),
+      child: Container(
+        color: color,
+        width: imgWidth?.toDouble(),
+        height: imgHeight?.toDouble(),
+        child: Image(
+          image: MLocalCacheNetworkImage(url),
+          loadingBuilder: (BuildContext context, Widget child, ImageChunkEvent? loadingProgress) {
+            return loadingProgress == null ? child : Image.memory(mTransparentImage);
+          },
+          errorBuilder: (BuildContext context, Object exception, StackTrace? stackTrace) {
+            return isShowError
+                ? Center(
+                    child: Icon(
+                      Icons.error,
+                      color: Colors.black.withOpacity(0.08),
+                    ),
+                  )
+                : Container();
+          },
+          fit: boxFit ?? BoxFit.cover,
+        ),
       ),
     );
   }
