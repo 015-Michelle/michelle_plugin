5b1a5220d92faaf421b98e036cece7e60960b84b
tm <tm@work.com>
2022-02-18 15:41:37 +0800

    add MType.image

--- lib/model/m_base_widget_model.dart
+++ lib/model/m_base_widget_model.dart
@@ -1,4 +1,5 @@
 import 'package:michelle_plugin/constant/m_constant.dart';
+import 'package:michelle_plugin/model/m_image_model.dart';
 import 'package:michelle_plugin/model/m_text_model.dart';
 
 class MBaseWidgetModel {
@@ -6,10 +7,15 @@ class MBaseWidgetModel {
   MBaseWidgetModel({required this.mType});
 
   factory MBaseWidgetModel.parse(Map<String, dynamic> json) {
-    String type = json['type'];
+    String type = json['mType'];
+    var config = json['config'];
     switch (type) {
       case MType.text:
-        return MTextModel.fromJson(json['config']);
+        return MTextModel.fromJson(config);
+      case MType.image:
+        return MImageModel.fromJson(config);
+      case MType.product:
+        return MImageModel.fromJson(config);
       default: //todo
         throw Exception();
     }
