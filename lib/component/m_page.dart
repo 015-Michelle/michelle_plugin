391b5c07f3a933b16404b4e6ef6485270dfabe84
tm <tm@work.com>
2022-02-18 10:36:33 +0800

    finish m_page

---
+++ lib/component/m_page.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:michelle_plugin/component/render.dart';
+import 'package:michelle_plugin/model/m_page_model.dart';
+import 'package:michelle_plugin/utils/color_util.dart';
+
+class MPage extends StatefulWidget {
+  final MPageModel pageModel;
+  final bool isFullPage;
+
+  const MPage({Key? key, required this.pageModel, required this.isFullPage}) : super(key: key);
+
+  @override
+  _MPageState createState() => _MPageState();
+}
+
+class _MPageState extends State<MPage> {
+  MPageModel get pageModel => widget.pageModel;
+  bool get isFullPage => widget.isFullPage;
+
+  @override
+  Widget build(BuildContext context) {
+    return isFullPage
+        ? Scaffold(
+            backgroundColor: ColorUtil.parseRGBA(pageModel.pageInfoModel.backgroundColor),
+            appBar: AppBar(
+              title: Text(pageModel.pageInfoModel.title),
+            ),
+            body: SingleChildScrollView(
+              physics: const BouncingScrollPhysics(),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: pageModel.layout.map((e) => MRender.makeupWidget(e)).toList(),
+              ),
+            ),
+          )
+        : Container(
+            decoration: BoxDecoration(
+              color: ColorUtil.parseRGBA(pageModel.pageInfoModel.backgroundColor),
+              border: Border.all(color: const Color(0x00ffffff), width: 0.0),
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: pageModel.layout.map((e) => MRender.makeupWidget(e)).toList(),
+            ),
+          );
+  }
+}
