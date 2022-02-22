da036da7228ca3247b0a833dbbf657923cd0bf3b
tm <tm@work.com>
2022-02-22 22:04:56 +0800

    add banner&kingkong_area

--- lib/model/m_base_widget_model.dart
+++ lib/model/m_base_widget_model.dart
@@ -1,5 +1,7 @@
 import 'package:michelle_plugin/constant/m_constant.dart';
+import 'package:michelle_plugin/model/m_banner_model.dart';
 import 'package:michelle_plugin/model/m_image_model.dart';
+import 'package:michelle_plugin/model/m_kingkong_area_model.dart';
 import 'package:michelle_plugin/model/m_product_model.dart';
 import 'package:michelle_plugin/model/m_text_model.dart';
 
@@ -17,6 +19,10 @@ class MBaseWidgetModel {
         return MImageModel.fromJson(config);
       case MType.product:
         return MProductModel.fromJson(config);
+      case MType.banner:
+        return MBannerModel.fromJson(config);
+      case MType.kingKongArea:
+        return MKingKongAreaModel.fromJson(config);
       default: //todo
         throw Exception();
     }
